d3ab69becaa05aafe322fba2d555eaa0867d65f9
r00t32 <aryanmisra4@gmail.com>
2016-09-07 20:22:10 -0400

    Add files via upload

---
+++ keith2.py
@@ -0,0 +1,23 @@
+
+def checker(num):
+    stringnum = str(num)
+    arraynum = list(map(int, stringnum))
+    sumnum = sum(arraynum)
+
+    while sumnum < num:
+        arraynum = arraynum[1:] + [sumnum]
+        sumnum = sum(arraynum)
+
+    if (sumnum == num):
+        print str(num)  + " is a keith number"
+        
+print "***DISCLAIMER***\nIF THE NUMBER IT IS CHECKING GOES ABOVE 100,000 YOU WILL EXPERIENCE INTENSE LAG"
+count = int(raw_input("\nAt what number would you like to stop at?: "))
+value = 10
+while (value <= count):
+    
+    checker(value)
+    value += 1
+
+print "Done!"
+
